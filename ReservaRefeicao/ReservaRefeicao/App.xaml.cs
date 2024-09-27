36251ea5910788374522b0e2bfae04f43ad5b953
allisson <alirod.santos@gmail.com>
2024-09-27 20:46:57 -0300

    First commit

---
+++ ReservaRefeicao/ReservaRefeicao/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ReservaRefeicao
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
